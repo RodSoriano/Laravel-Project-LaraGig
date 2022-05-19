ccdd5e55639a7940114ddb22858411a5410941d4
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-05-19 11:19:52 -0600

    Listing model created

---
+++ app/Models/Listing.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Models;
+
+class Listing
+    {
+        public static function all()
+            {
+                return [ 
+                            [
+                                'id'            => 1,
+                                'title'         => 'listing one',
+                                'description'   => 'Wenas noches le de dios'
+                            ],
+                            [
+                                'id'            => 2,
+                                'title'         => 'listing two',
+                                'description'   => 'Wenas noches le de dios x2'
+                            ]
+                        ];
+            }
+
+        public static function find($id)
+            {
+                $listings = self::all();
+
+                foreach($listings as $listing)
+                    {
+                        if($listing['id'] == $id)
+                            {
+                                return $listing;
+                            }
+                    }
+            }
+    }
+
+#min41.10
+?>
