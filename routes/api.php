2edddc76f067ab96409d78c584a971338d90436f
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-05-19 11:19:31 -0600

    changes made on files, delete and comments

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+#API reference
+
+// Route::get('/post', function()
+//     {
+//         return response()->json(
+//             [
+//                 'posts' => 
+//                     [
+//                         [
+//                             'title' => 'Post One'
+//                         ]
+//                     ]
+//             ]);
+//     });
