fef91f0c6d0f1e301fca8c6c7153668a34c49114
Rodrigo Soriano <rsoriano.jose@gmail.com>
2022-05-21 11:58:52 -0600

    update

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -4,7 +4,7 @@
 
 @foreach($listings as $listing)
     <h2>
-        <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
+        <a href="/Laravel-Project-LaraGig/public/listings/{{$listing['id']}}">{{$listing['title']}}</a>
     </h2>
     <p>{{$listing['description']}}</p>
 @endforeach
